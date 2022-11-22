4bfeabca9b71714b9e23eb58985b9f7aa9b91dad
Andrea Colleoni <andrea.colleoni@outlook.it>
2022-11-22 18:07:03 +0100

    aggiunta addIndirizzo

--- eclipse/corso-jee-01/src/main/java/it/corso/jpa/AppJPA.java
+++ eclipse/corso-jee-01/src/main/java/it/corso/jpa/AppJPA.java
@@ -15,18 +15,18 @@ public class AppJPA {
 		
 		EntityManager em = emf.createEntityManager();
 		
-//		Persona p = new Persona();
-//		p.setNome("Mario");
-//		p.setCognome("Rossi");
-//		p.setEmail("mario@rossi");
-//		p.setIndirizzi(new ArrayList<Indirizzo>());
-//		Indirizzo i = new Indirizzo();
-//		i.setCitta("Roma");
-//		i.setCivico(1);
-//		i.setVia("via");
-//		// creo un doppio collegamento tra le entità
-//		i.setPersona(p);
-//		p.getIndirizzi().add(i);
+		Persona p = new Persona();
+		p.setNome("Mario");
+		p.setCognome("Rossi");
+		p.setEmail("mario@rossi");
+
+		Indirizzo i = new Indirizzo();
+		i.setCitta("Roma");
+		i.setCivico(1);
+		i.setVia("via");
+		
+		// creo un doppio collegamento tra le entità
+		p.addIndirizzo(i);
 //		
 //		em.getTransaction().begin();
 //		// sincronizzazione con il DB (racchiusa in una TX)
--- eclipse/corso-jee-01/src/main/java/it/corso/jpa/Persona.java
+++ eclipse/corso-jee-01/src/main/java/it/corso/jpa/Persona.java
@@ -1,15 +1,17 @@
 package it.corso.jpa;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-@Entity
+@Entity(name = "TBL_PRS")
 public class Persona implements Serializable {
 
 	/**
@@ -17,7 +19,7 @@ public class Persona implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	// POJO usando JavaBean
-	
+	@Column(name = "NM001")
 	private String nome;
 	private String cognome;
 	@Id
@@ -25,6 +27,14 @@ public class Persona implements Serializable {
 	
 	@OneToMany(mappedBy = "persona", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Indirizzo> indirizzi;
+	
+	public void addIndirizzo(Indirizzo i) {
+		if (indirizzi == null) {
+			indirizzi = new ArrayList<Indirizzo>();
+		}
+		indirizzi.add(i);
+		i.setPersona(this);
+	}
 	
 	/**
 	 * @return the nome
